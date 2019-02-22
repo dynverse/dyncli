043d9d10c2c473e05780793a4ab326e4923deb48
Robrecht Cannoodt <rcannood@gmail.com>
2019-02-22 16:10:19 +0100

    work on main functions

--- R/main.R
+++ R/main.R
@@ -5,6 +5,11 @@ main <- function() {
   args <- c("ti", "--seed", "4", "--output", "/ti/output/")
   args <- c("api")
 
+  case_when(
+    length(args) == 0 ~ print_help(),
+    args[[1]] == "ti" ~ main_ti(args[-1]),
+    args[[1]] == "tidirect" ~
+  )
 }
 
 main_api <- function(args) {
@@ -20,8 +25,12 @@ main_api <- function(args) {
   )
 }
 
+print_help <- function() {
+
+}
+
 # definition_location <- "~/Workspace/dynverse/methods/ti_paga/definition.yml"
-# args <- c("--seed", "4", "--input", "/ti/input/expression.csv", "--output", "/ti/output/output.h5", "--params", "/ti/input/params.yaml", "--n_comps", "50")
+# args <- c("--seed", "4", "--expression", "/ti/input/expression.csv", "--counts", "/ti/input/counts.csv", "--output", "/ti/output/output.h5", "--params", "/ti/input/params.yaml", "--n_comps", "50")
 #' @import optparse
 #' @importFrom dynwrap create_ti_method_definition
 main_ti <- function(args, definition_location = "/code/definition.yml") {
@@ -31,23 +40,55 @@ main_ti <- function(args, definition_location = "/code/definition.yml") {
     OptionParser(usage = paste0("docker run -v ~/ti_working_dir:/ti ", definition$container$docker)) %>%
     add_option(c("-v", "--verbose"), action = "store_true", default = FALSE, help = "Print extra output") %>%
     add_option(c("--seed"), type = "integer", default = NA, help = "A seed to be set") %>%
-    add_option(c("--input"), type = "character", help = "Filename of the expression data, example: /ti/input/expression.h5") %>%
+    add_option(c("--expression"), type = "character", help = "Filename of expression data, example: /ti/input/expression.h5") %>%
+    add_option(c("--counts"), type = "character", help = "Filename of raw counts data, example: /ti/input/counts.h5") %>%
     add_option(c("--output"), type = "character", help = "Filename of the output data, example: /ti/output/output.h5") %>%
     add_option(c("--params"), type = "character", help = "A file containing method-specific parameters, example: /ti/input/params.yaml", default = NULL)
 
   parameters <- definition$parameters$parameters
+
   # generate documentation per parameter separately
-  for (pi in seq_along(parameters)) {
+  for (parameter in parameters) {
     # is there no function in optparse to do this cleanly?
     parser@options[[length(parser@options) + 1]] <-
-      dynparam::as_argparse(parameters[[pi]])
+      dynparam::as_argparse(parameter)
   }
 
   # generate documentation per input separately
   # definition$input
   # dynwrap::allowed_inputs
 
-  args <- parse_args(parser, args = args)
+  parsed_args <- parse_args(parser, args = args)
+
+  # convert parameter values to correct type
+  param_values <- list()
+  for (parameter in parameters) {
+    param_values[[parameter$id]] <- dynparam::argparse_trafo(parameter, parsed_args[[parameter$id]])
+  }
+
+  param_values$verbose <- parsed_args$verbose
+  param_values$seed <- parsed_args$seed
+
+  # read expression / counts
+  expression <- NULL # ...
+  counts <- NULL # ...
+
+  # read priors
+  # ...
+
+  # create dataset
+  dataset <-
+    dynwrap::wrap_expression(
+      expression = expression,
+      counts = counts
+    )
+
+  # write params to temporary file
+  data <- lst(
+    expression_file =
+    params = param_values
+  )
+
 
 
   ### WIP
@@ -64,4 +105,8 @@ main_ti <- function(args, definition_location = "/code/definition.yml") {
   source("/code/run.R")
 
   readr::write_rds(output, args$output)
+}
+
+main_ti_with_data <- function(data_file) {
+
 }
