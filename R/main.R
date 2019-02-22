34253b8e65c1e39c58df4496ec25b60ebdfc842c
Robrecht Cannoodt <rcannood@gmail.com>
2019-02-22 10:50:03 +0100

    working on cli parser

--- R/main.R
+++ R/main.R
@@ -1,80 +1,56 @@
 
-#' @import optparse
 #' @export
 main <- function() {
   args <- commandArgs(trailingOnly = TRUE)
   args <- c("ti", "--seed", "4", "--output", "/ti/output/")
   args <- c("api")
 
-  # definition <- dynmethods::ti_scorpius()
-  definition <- dynwrap::create_ti_method_container("dynverse/ti_gng")()
+}
+
+main_api <- function(args) {
+  defaults_yaml <- list(
+    "function" = list(
+      "name" = "ti",
+      "command" = "Rscript /code/run.R",
+      "pre-hook" = "/code/pre-hook.sh",
+      "post-hook" = "",
+      "arguments" = "",
+      "parameters" = ""
+    )
+  )
+}
+
+# definition_location <- "~/Workspace/dynverse/methods/ti_paga/definition.yml"
+# args <- c("--seed", "4", "--input", "/ti/input/expression.csv", "--output", "/ti/output/output.h5", "--params", "/ti/input/params.yaml", "--n_comps", "50")
+#' @import optparse
+#' @importFrom dynwrap create_ti_method_definition
+main_ti <- function(args, definition_location = "/code/definition.yml") {
+  definition <- dynwrap::create_ti_method_definition(filename = definition_location, return_function = FALSE)
 
   parser <-
-    OptionParser(usage = paste0("docker run ~/SOMEDIR:/ti ", definition$docker_repository)) %>%
+    OptionParser(usage = paste0("docker run -v ~/ti_working_dir:/ti ", definition$container$docker)) %>%
     add_option(c("-v", "--verbose"), action = "store_true", default = FALSE, help = "Print extra output") %>%
     add_option(c("--seed"), type = "integer", default = NA, help = "A seed to be set") %>%
-    add_option(c("--output"), type = "character", help = "Filename of the output data")
+    add_option(c("--input"), type = "character", help = "Filename of the expression data, example: /ti/input/expression.h5") %>%
+    add_option(c("--output"), type = "character", help = "Filename of the output data, example: /ti/output/output.h5") %>%
+    add_option(c("--params"), type = "character", help = "A file containing method-specific parameters, example: /ti/input/params.yaml", default = NULL)
 
-  # forbidden are forbidden combinations of parameters; e.g. xmin > xmax.
-  parameter_ids <-
-    names(definition$parameters) %>%
-    keep(~. != "forbidden")
-
+  parameters <- definition$parameters$parameters
   # generate documentation per parameter separately
-  for (parameter_id in parameter_ids) {
-    cat("Processing ", parameter_id, "\n", sep = "")
-    parameter <- definition$parameters[[parameter_id]]
-
-    description <-
-      parameter$description %>%
-      ifelse(!is.null(.), ., "") %>%     # use "" if no description is provided
-      str_replace_all("\n", "") %>%      # remove newlines
-      Hmisc::capitalize()                # capitalise sentences
-
-    range_text <-
-      case_when(
-        parameter$type == "discrete" ~ paste0("; values: {", paste0(sapply(parameter$values, deparse), collapse = ", "), "}"),
-        parameter$type %in% c("integer", "numeric") ~ paste0("; range: from ", deparse(parameter$lower), " to ", deparse(parameter$upper)),
-        TRUE ~ ""
-      )
-
-    default_str <-
-      case_when(
-        parameter$type %in% c("integer", "numeric", "discrete", "logical") ~ paste0(" (default: ", deparse(parameter$default), range_text, ")"),
-        TRUE ~ ""
-      )
-
-    default_value <-
-      if (parameter$type %in% c("discrete", "integer", "logical", "numeric")) {
-        parameter$default
-      } else {
-        deparse(parameter$default)
-      }
-
-    parser <- parser %>% add_option(
-      opt_str = paste0("--", parameter_id),
-      type = case_when(
-        parameter$type == "discrete" ~ "character",
-        parameter$type %in% c("integer", "logical", "numeric") ~ parameter$type,
-        TRUE ~ "character"
-      ),
-      default = ,
-      help = paste0(parameter$type, "; ", description, default_str) %>%
-        gsub("\\\\link\\[[a-zA-Z0-9_:]*\\]\\{([^\\}]*)\\}", "\\1", .) # substitute \link[X](Y) with just Y
-    )
+  for (pi in seq_along(parameters)) {
+    # is there no function in optparse to do this cleanly?
+    parser@options[[length(parser@options) + 1]] <-
+      dynparam::as_argparse(parameters[[pi]])
   }
 
   # generate documentation per input separately
-  definition$input
-  dynwrap::allowed_inputs
+  # definition$input
+  # dynwrap::allowed_inputs
+
+  args <- parse_args(parser, args = args)
 
 
-  args <- parse_args(parser, "-h")
-
-  args <- parse_args(parser)
-
-  #   add_option(c("--expression"), type = "character", help = "Filename of the expression data") %>%
-
+  ### WIP
   print(args)
 
   if (!is.null(args$seed) && !is.na(args$seed)) set.seed(args$seed)
@@ -88,8 +64,4 @@ main <- function() {
   source("/code/run.R")
 
   readr::write_rds(output, args$output)
-}
-
-main_ti <- function(args) {
-
 }
