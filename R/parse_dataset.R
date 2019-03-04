a45bf774509a2c705c8edfef99f79b17a8aef5c4
Robrecht Cannoodt <rcannood@gmail.com>
2019-03-04 13:03:03 +0100

    fix read_h5 and use h5 to read in own files

--- R/parse_dataset.R
+++ R/parse_dataset.R
@@ -1,5 +1,5 @@
 #' @importFrom fs is_file
-#' @importFrom hdf5r H5File
+#' @importFrom hdf5r H5File h5attr_names
 #' @importFrom Matrix sparseMatrix Matrix t
 parse_dataset <- function(x, loom_expression_layer = NULL) {
   assert_that(fs::is_file(x))
@@ -58,36 +58,11 @@ parse_dataset <- function(x, loom_expression_layer = NULL) {
   } else if (grepl("\\.h5$", x)) {
     file_h5 <- H5File$new(x, mode = "r")
 
-    if (file_h5 %has_names% c("expression", "counts", "cell_ids", "feature_ids")) {
+    if (file_h5 %has_names% c("data", "names") && "object_class" %in% h5attr_names(file_h5)) {
       ##########################
       ###       OWN H5       ###
       ##########################
-      cell_ids <- file_h5[["cell_ids"]]
-      feature_ids <- file_h5[["feature_ids"]]
-
-      dims <- c(length(cell_ids), length(feature_ids))
-      dimnames <- list(cell_ids, feature_ids)
-
-      counts_data <- file_h5[["counts"]]
-      expression_data <- file_h5[["expression"]]
-
-      counts <- with(counts_data, Matrix::sparseMatrix(
-        i = i,
-        p = p,
-        x = x,
-        dims = dims,
-        dimnames = dimnames
-      ))
-
-      expression <- with(expression_data, Matrix::sparseMatrix(
-        i = i,
-        p = p,
-        x = x,
-        dims = dims,
-        dimnames = dimnames
-      ))
-
-      ## TODO: support other inputs with `extra_input` as well!
+      .read_h5(file_h5)
 
     } else if (file_h5 %has_names% "matrix" && file[["matrix"]] %has_names% c("barcodes", "data", "features", "indices", "indptr", "shape")) {
       ##########################
@@ -104,7 +79,8 @@ parse_dataset <- function(x, loom_expression_layer = NULL) {
           dimnames = list(
             subfile[["features/id"]][],
             subfile[["barcodes"]][]
-          )
+          ),
+          index1 = FALSE
         ) %>%
         Matrix::t()
     } else if (length(names(file_h5)) == 1 && file_h5[[names(file_h5)]] %has_names% c("barcodes", "data", "genes", "indices", "indptr", "shape")) {
@@ -122,7 +98,8 @@ parse_dataset <- function(x, loom_expression_layer = NULL) {
           dimnames = list(
             subfile[["genes"]][],
             subfile[["barcodes"]][]
-          )
+          ),
+          index1 = FALSE
         ) %>%
         Matrix::t()
     }
@@ -139,6 +116,7 @@ parse_dataset <- function(x, loom_expression_layer = NULL) {
 
 normalise <- function(counts) {
   # TODO: provide better normalisation :(
+  # TODO: Also print out warning that better normalisation should be added
   expr <- counts
   expr@x <- log2(expr@x + 1)
   expr
@@ -152,9 +130,9 @@ write_h5 <- function(x, file) {
   file_h5$close_all()
 }
 
-
+#' @importFrom hdf5r h5attr<-
 .write_h5 <- function(x, file_h5, name) {
-  cat("Processing ", name, "\n", sep = "")
+  cat("Processing ", file_h5$get_obj_name(), "/", name, "\n", sep = "")
   if (any(grepl("^[dlniz]..Matrix$", class(x)))) {
     ipx <- as(x, "dgCMatrix")
     file_h5$create_group(name)
@@ -174,31 +152,26 @@ write_h5 <- function(x, file) {
     file_h5$create_group(name)
     subfile <- file_h5[[name]]
     h5attr(subfile, "object_class") <- "dense_matrix"
-    subfile[["rownames"]] <- rownames(x)
-    subfile[["colnames"]] <- colnames(x)
-    subfile[["matrix"]] <- x
+    if (!is.null(rownames(x))) subfile[["rownames"]] <- rownames(x)
+    if (!is.null(colnames(x))) subfile[["colnames"]] <- colnames(x)
+    subfile[["data"]] <- x
   } else if (is.data.frame(x)) {
     file_h5$create_group(name)
     subfile <- file_h5[[name]]
     h5attr(subfile, "object_class") <- "data_frame"
-    walk2(
-      x,
-      names(x),
-      function(xx, xn) {
-        .write_h5(xx, subfile, xn)
-      }
-    )
-  } else if (is.atomic(x)) {
-    if (is.null(names(x))) {
-      file_h5[[name]] <- x
-      h5attr(file_h5[[name]], "object_class") <- "vector"
-    } else {
-      file_h5$create_group(name)
-      subfile <- file_h5[[name]]
-      h5attr(subfile, "object_class") <- "named_vector"
-      subfile[["names"]] <- names(x)
-      subfile[["values"]] <- x
+    subfile[["rownames"]] <- rownames(x)
+    subfile[["colnames"]] <- colnames(x)
+    subfile$create_group("data")
+    subsubfile <- subfile[["data"]]
+    for (xn in names(x)) {
+      subsubfile[[xn]] <- x[[xn]]
     }
+  } else if (is.atomic(x)) {
+    file_h5$create_group(name)
+    subfile <- file_h5[[name]]
+    h5attr(subfile, "object_class") <- "vector"
+    if (!is.null(names(x))) subfile[["names"]] <- names(x)
+    subfile[["data"]] <- x
   } else if (is.list(x)) {
     if (name == "") {
       subfile <- file_h5
@@ -206,14 +179,18 @@ write_h5 <- function(x, file) {
       file_h5$create_group(name)
       subfile <- file_h5[[name]]
     }
-    h5attr(file_h5, "object_class") <- "list"
-    walk2(
-      x,
-      names(x),
-      function(xx, xn) {
-        .write_h5(xx, subfile, xn)
-      }
-    )
+    h5attr(subfile, "object_class") <- "list"
+    subfile[["class"]] <- class(x)
+    if (!is.null(names(x))) subfile[["names"]] <- names(x)
+
+    subfile$create_group("data")
+    subsubfile <- subfile[["data"]]
+
+    if (is.null(names(x))) names(x) <- paste0("elem", seq_along(x))
+
+    for (xn in names(x)) {
+      .write_h5(x[[xn]], subsubfile, xn)
+    }
   }
 }
 
@@ -227,61 +204,96 @@ read_h5 <- function(file) {
   lis
 }
 
-#' @importFrom hdf5r h5attr_names
+#' @importFrom hdf5r h5attr_names h5attr
 .read_h5 <- function(file_h5) {
-  if ("object_class" %in% h5attr_names(file_h5)) {
-    object_class <- h5attr(file_h5, "object_class")
+  cat("Reading ", file_h5$get_obj_name(), "\n", sep = "")
+  if (!"object_class" %in% h5attr_names(file_h5)) {
+    stop("Object class not found (path=", file_h5$get_filename(), ", obj_name=", file_h5$get_obj_name())
+  }
+  object_class <- h5attr(file_h5, "object_class")
 
-    if (object_class == "list") {
-      nms <- names(file_h5)
-      out <- map(nms, function(nm) {
-        cat("Recursing ", nm, "\n", sep = "")
-        .read_h5(file_h5[[nm]])
-      })
-      names(out) <- nms
-      out
-    } else if (object_class == "named_vector") {
-      nms <- file_h5[["names"]][]
-      vls <- file_h5[["values"]][]
-      set_names(vls, nms)
-    } else if (object_class == "data_frame") {
-      nms <- names(file_h5)
-      out <- map(nms, ~ .read_h5(file_h5[[.]]))
-      names(out) <- nms
-      data.frame(out, check.names = FALSE, stringsAsFactors = FALSE)
-    } else if (object_class == "dense_matrix") {
-      mat <- file_h5[["matrix"]][,]
-      rownames(mat) <- file_h5[["rownames"]][]
-      colnames(mat) <- file_h5[["colnames"]][]
-    } else if (object_class == "sparse_matrix") {
-      i <- file_h5[["i"]][]
-      p <- file_h5[["p"]][]
-      x <- file_h5[["x"]][]
-      dims <- file_h5[["dims"]][]
+  ## VECTOR ##
+  if (object_class == "vector") {
+    data_file <- file_h5[["data"]]
 
-      rn <-
-        if ("rownames" %in% names(file_h5)) {
-          file_h5[["rownames"]][]
-        } else {
-          NULL
-        }
+    # out <- data_file[]
+    # if ("names" %in% names(file_h5)) names(out) <- file_h5[["names"]][]
 
-      cn <-
-        if ("colnames" %in% names(file_h5)) {
-          file_h5[["colnames"]][]
-        } else {
-          NULL
-        }
+    # workaround
+    out <- .read_h5_vec(data_file)
+    if ("names" %in% names(file_h5)) names(out) <- .read_h5_vec(file_h5[["names"]])
 
-      Matrix::sparseMatrix(
-        i = i,
-        p = p,
-        x = x,
-        dims = dims,
-        dimnames = list(rn, cn)
-      )
-    }
+    out
+
+  ## DENSE MATRIX ##
+  } else if (object_class == "dense_matrix") {
+    out <- file_h5[["data"]][,]
+    # if ("rownames" %in% names(file_h5)) rownames(out) <- file_h5[["rownames"]][]
+    # if ("colnames" %in% names(file_h5)) colnames(out) <- file_h5[["colnames"]][]
+
+    # workaround
+    if ("rownames" %in% names(file_h5)) rownames(out) <- .read_h5_vec(file_h5[["rownames"]])
+    if ("colnames" %in% names(file_h5)) colnames(out) <- .read_h5_vec(file_h5[["colnames"]])
+    out
+
+  ## LIST ##
+  } else if (object_class == "list") {
+    subfile <- file_h5[["data"]]
+
+    has_names <- "names" %in% names(file_h5)
+    # nms <- if (has_names) file_h5[["names"]][] else names(subfile)
+    # workaround
+    nms <- if (has_names) .read_h5_vec(file_h5[["names"]]) else names(subfile)
+
+    out <- map(nms, ~.read_h5(subfile[[.]]))
+    if (has_names) names(out) <- nms
+    if ("class" %in% names(file_h5)) class(out) <- file_h5[["class"]][]
+
+    out
+
+  ## DATA FRAME ##
+  } else if (object_class == "data_frame") {
+    # rownames <- file_h5[["rownames"]][]
+    # colnames <- file_h5[["colnames"]][]
+
+    # workaround
+    rownames <- .read_h5_vec(file_h5[["rownames"]])
+    colnames <- .read_h5_vec(file_h5[["colnames"]])
+
+    data <- file_h5[["data"]]
+    out <- map(colnames, ~ .read_h5_vec(data[[.]])) %>% data.frame(check.names = FALSE, stringsAsFactors = FALSE)
+    rownames(out) <- rownames
+    colnames(out) <- colnames
+    out
+
+  ## SPARSE MATRIX ##
+  } else if (object_class == "sparse_matrix") {
+    i <- file_h5[["i"]][]
+    p <- file_h5[["p"]][]
+    x <- file_h5[["x"]][]
+    dims <- file_h5[["dims"]][]
+
+    # rn <- if ("rownames" %in% names(file_h5)) file_h5[["rownames"]][] else NULL
+    # cn <- if ("colnames" %in% names(file_h5)) file_h5[["colnames"]][] else NULL
+
+    rn <- if ("rownames" %in% names(file_h5)) .read_h5_vec(file_h5[["rownames"]]) else NULL
+    cn <- if ("colnames" %in% names(file_h5)) .read_h5_vec(file_h5[["colnames"]]) else NULL
+
+    Matrix::sparseMatrix(
+      i = i,
+      p = p,
+      x = x,
+      dims = dims,
+      dimnames = list(rn, cn),
+      index1 = FALSE
+    )
+  }
+}
+
+.read_h5_vec <- function(file_h5)
+  # workaround for https://github.com/hhoeflin/hdf5r/issues/118
+  if (file_h5$dims == 0 && "H5T_STRING" %in% class(file_h5$get_type())) {
+    character(0)
   } else {
     file_h5[]
   }
-}
