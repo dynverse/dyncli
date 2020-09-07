f67589055748de465ab1079708163642b6564d1d
Robrecht Cannoodt <rcannood@gmail.com>
2020-09-07 13:02:41 +0200

    remove unused dependency

--- R/package.R
+++ R/package.R
@@ -9,7 +9,6 @@
 #' @import assertthat
 #' @importFrom tibble is_tibble as_tibble as_data_frame tibble data_frame enframe deframe lst tribble rownames_to_column column_to_rownames
 #' @importFrom magrittr %<>% %$% set_rownames set_colnames
-#' @importFrom glue glue
 #'
 #' @docType package
 #' @name dyncli
